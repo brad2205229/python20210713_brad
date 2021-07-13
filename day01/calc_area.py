b46f3ca8f120f66944da3b7312dd5f2307b085cf
brad2205229 <brad2205229@gmail.com>
2021-07-13 22:08:41 +0800

    python

---
+++ day01/calc_area.py
@@ -0,0 +1,16 @@
+import math
+
+if __name__ == '__main__':
+    print("pi", 3.14)
+    print("pi", math.pi)
+    print("1.計算圓面積")
+    r = 35
+    area = r**2 * math.pi
+    print("半徑: %d 圓面積: %.2f" % (r, area))
+    print("半徑: %d 圓面積: %s" % (r, format(area, ",")))
+    print("半徑: %d 圓面積: %s" % (r, format(int(area*100)/100, ",")))
+    print("2.計算球面積")
+    r = 35
+    vol = (4/3) * (r**3 * math.pi)
+    print("半徑: %d 球體積: %.2f" % (r, vol))
+    print("半徑: %d 球體積: %s" % (r, format(int(vol*100)/100,  ",")))
